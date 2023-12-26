ce89f0078e121c4efdef493b0e1e4a12b319cfb1
Oliver Daniel Carvajal <oliverdanielcarvajal@hotmail.com>
2023-12-25 20:48:25 -0500

    fix: desktop width

--- src/components/ui/currentBook.tsx
+++ src/components/ui/currentBook.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export const CurrentBook = ({ book }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center h-full w-screen lg:min-w-[500px] max-w-[100vw]">
+    <div className="flex flex-col items-center justify-center h-full w-screen lg:w-auto lg:min-w-[500px] max-w-[100vw]">
       <h2 className=" text-4xl md:text-6xl font-bold text-white">{book}</h2>
     </div>
   );
