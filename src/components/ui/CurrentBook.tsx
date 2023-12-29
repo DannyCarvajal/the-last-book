f2cce0d1e8f0bc6d17266a41f735dba45cdd9f1f
Oliver Daniel Carvajal <oliverdanielcarvajal@hotmail.com>
2023-12-29 14:36:49 -0500

    chore: add them back

---
+++ src/components/ui/CurrentBook.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  book: string;
+};
+
+export const CurrentBook = ({ book }: Props) => {
+  return (
+    <div className="flex flex-col items-center justify-center h-full w-screen lg:w-auto lg:min-w-[500px] max-w-[100vw]">
+      <h2 className=" text-4xl md:text-6xl font-bold text-white">{book}</h2>
+    </div>
+  );
+};
