9fcf149e43cb0b09562011154fed2c4a7c4c6c94
Oliver Daniel Carvajal <oliverdanielcarvajal@hotmail.com>
2024-01-13 19:50:59 -0500

    chore: get inital data from local

--- src/hooks/api/useUser.ts
+++ src/hooks/api/useUser.ts
@@ -1,10 +1,19 @@
 import useSWR from "swr";
 import { User } from "@/models/user";
+import { useEffect, useState } from "react";
+import { getLocalStorageOrCookiesUser } from "@/utils/session";
 
 export const useUser = () => {
   const { data, error, isLoading, mutate } = useSWR<User[]>(`/api/user`);
+  const [localUser, setLocalUser] = useState<User | null>({ username: null, userId: null });
 
   const userData = data?.[0] || null;
+
+  /* Get fast user from localStorage */
+  useEffect(() => {
+    const localUser = getLocalStorageOrCookiesUser();
+    setLocalUser(localUser);
+  }, []);
 
   /* Show local save data if user have not loaded yet */
   const baseUser: User = {
@@ -15,8 +24,7 @@ export const useUser = () => {
       country: null,
       flag: null,
     },
-    userId: null,
-    username: null,
+    ...localUser,
   };
 
   const currUser = userData ? userData : baseUser;
